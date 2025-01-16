2000d4f47af93e2d60165eaa43f54544c14dacf4
imleznupar <jasminelu23@gmail.com>
2025-01-16 13:23:05 -0600

    setup app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
-const express = require("express");
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import express from 'express';
+
+// config express
 const app = express();
 const port = 3000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World! asdjfa;sdkfj");
+// config gemini
+dotenv.config();
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+async function run(prompt) {
+  const result = await model.generateContent(prompt);
+  console.log(result.response.text());
+  return (result.response.text());
+}
+
+// setup app
+app.get("/", async function (req, res) {
+  try {
+    const prompt = "how are you doing";
+    const response = await run(prompt);
+    res.send(response);
+  } catch (error) {
+    console.error("Error generating content:", error);
+    res.status(500).send("could not generate a response");
+  }
 });
 
 app.listen(port, function () {
