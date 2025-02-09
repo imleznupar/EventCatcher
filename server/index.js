b66ec1e78b1bb55dcc492e8471b78410ba88461f
imleznupar <jasminelu23@gmail.com>
2025-02-09 17:02:46 -0600

    set up docker

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 
 // config express
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: 'https://mail.google.com',
